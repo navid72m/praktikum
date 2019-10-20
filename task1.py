d292f0762a5ea3327134c17dda4bf4462d2b1f2e
navid <navid72m@gmail.com>
2019-10-20 21:38:13 +0200

    task1 & 2 complete

--- task1.py
+++ task1.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot
 import numpy
 import cv2
 import time
+import math
 
 class Panel:
 
@@ -22,11 +23,15 @@ class Panel:
 
         self.Xpedestrian = Xpedestrian
         self.Ypedestrian = Ypedestrian
+        self.Xtarget = Xtarget
+        self.Ytarget = Ytarget
         self.drawGrid()
 
         self.draw.rectangle((Xpedestrian*self.step_size,Ypedestrian*self.step_size , (Xpedestrian+1)*self.step_size,(Ypedestrian+1)*self.step_size ), fill=134, outline=(134) ,width=self.step_size)
         fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
-        self.draw.text((Xpedestrian*self.step_size,Ypedestrian*self.step_size), "T", font=fnt, fill=0)
+        self.draw.text((Xpedestrian*self.step_size,Ypedestrian*self.step_size), "P", font=fnt, fill=0)
+
+        self.drawRec(Xtarget, Ytarget, "T", 134)
         # cv2.imshow("show", numpy.array(self.image))
 
     def drawGrid(self):
@@ -46,21 +51,53 @@ class Panel:
         fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
         self.draw.text((X*self.step_size,Y*self.step_size), text, font=fnt, fill=0)
 
-    def moveRect(self, count):
-        count = 5
-        while (count != 0):
-            self.drawRec(self.Xpedestrian + count,self.Ypedestrian, "P",134)
-            print(self.Xpedestrian)
-            cv2.imshow("show", numpy.array(self.image))
-            cv2.waitKey(1000)
-            count = count -1
-            self.drawRec(self.Xpedestrian + count+1,self.Ypedestrian, "",255)
-            self.drawGrid()
+    def moveRect(self, X_new, Y_new, X_old, Y_old):
+
+        self.drawRec( X_new, Y_new, "P",134)
+        print(X_new)
+        if X_new != X_old or Y_old != Y_new:
+            self.drawRec(X_old,Y_old, "",255)
+        self.drawGrid()
+        cv2.imshow("show", numpy.array(self.image))
+        cv2.waitKey(1000)
+
 
         # del self.draw
 
+    def dist(self,p1,p2):
+        return math.sqrt( abs(p1[0]-p2[0])**2 +  abs(p1[1]-p2[1])**2)
+
+    def moveToTarget(self,pCord, tCord):
+
+        pX=pCord[0]
+        pY=pCord[1]
+        Xnew = pCord[0]
+        Ynew = pCord[1]
+        d=-1
+        d1 = self.dist([pX-1, pY] , tCord)
+        d2 = self.dist([pX+1,pY], tCord)
+        d3 = self.dist([pX,pY-1], tCord)
+        d4 = self.dist([pX,pY+1], tCord)
+        minD = min([d1,d2,d3,d4])
+        print(minD)
+        if minD != 0:
+            if minD == d1:
+                Xnew = pX-1
+            elif minD == d2:
+                Xnew = pX+1
+            elif minD == d3:
+                Ynew=pY-1
+            else:
+                Ynew=pY+1
+
+        self.moveRect(Xnew, Ynew, pX, pY)
+        return Xnew, Ynew
 
 
-p = Panel(4, 24, 25, 25)
-p.moveRect(5)
+
+p = Panel(2, 23, 25, 25)
+
+for i in range(0, 25):
+    p.Xpedestrian, p.Ypedestrian = p.moveToTarget([p.Xpedestrian, p.Ypedestrian], [p.Xtarget, p.Ytarget])
+
 cv2.waitKey(0)
