96028b9021e2f7b6a9a5be07e01c571fd2861c7f
navid <navid72m@gmail.com>
2019-10-20 16:09:25 +0200

    moving pedestrian

--- task1.py
+++ task1.py
@@ -4,8 +4,9 @@ import numpy
 import cv2
 import time
 
-if __name__ == '__main__':
-    step_count = 25
+class Panel:
+
+    step_count = 26
     height = 600
     width = 600
     image = Image.new(mode='L', size=(height, width), color=255)
@@ -16,31 +17,46 @@ if __name__ == '__main__':
     y_end = image.height
     step_size = int(image.width / step_count)
 
-    for x in range(0, image.width, step_size):
-        line = ((x, y_start), (x, y_end))
-        draw.line(line, fill=128)
+    def __init__(self, Xpedestrian, Ypedestrian, Xtarget, Ytarget):
 
-    x_start = 0
-    x_end = image.width
 
-    for y in range(0, image.height, step_size):
-        line = ((x_start, y), (x_end, y))
-        draw.line(line, fill=128)
+        self.Xpedestrian = Xpedestrian
+        self.Ypedestrian = Ypedestrian
+        for x in range(0, self.image.width, self.step_size):
+            line = ((x, self.y_start), (x, self.y_end))
+            self.draw.line(line, fill=128)
 
-    draw.rectangle((5*step_size,10*step_size , 6*step_size,11*step_size ), fill=134, outline=(134) ,width=step_size)
-    fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
-    draw.text((5*step_size,10*step_size), "P", font=fnt, fill=0)
+        x_start = 0
+        x_end = self.image.width
 
-    count = 5
-    while (count != 0):
-        draw.rectangle(((5 +count)*step_size,(10 + count)*step_size , (6 + count)*step_size,11*step_size ), fill=134, outline=(134) ,width=step_size)
+        for y in range(0, self.image.height, self.step_size):
+            line = ((x_start, y), (x_end, y))
+            self.draw.line(line, fill=128)
+
+        self.draw.rectangle((Xpedestrian*self.step_size,Ypedestrian*self.step_size , (Xpedestrian+1)*self.step_size,(Ypedestrian+1)*self.step_size ), fill=134, outline=(134) ,width=self.step_size)
         fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
-        draw.text((5*step_size,10*step_size), "P", font=fnt, fill=0)
-        cv2.imshow("show", numpy.array(image))
-        cv2.waitKey(1000)
-        count = count -1
+        self.draw.text((Xpedestrian*self.step_size,Ypedestrian*self.step_size), "T", font=fnt, fill=0)
+        # cv2.imshow("show", numpy.array(self.image))
+
+    def drawRec(self, X, Y, text, color):
+        self.draw.rectangle((X*self.step_size,Y*self.step_size , (X+1)*self.step_size,(Y+1)*self.step_size ), fill=color, outline=(color) ,width=self.step_size)
+        fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
+        self.draw.text((X*self.step_size,Y*self.step_size), text, font=fnt, fill=0)
+
+    def moveRect(self, count):
+        count = 5
+        while (count != 0):
+            self.drawRec(self.Xpedestrian + count,self.Ypedestrian, "P",134)
+            print(self.Xpedestrian)
+            cv2.imshow("show", numpy.array(self.image))
+            cv2.waitKey(1000)
+            count = count -1
+            self.drawRec(self.Xpedestrian + count+1,self.Ypedestrian, "",255)
+
+        # del self.draw
 
 
 
-    del draw
-    cv2.waitKey(0)
+p = Panel(4, 24, 25, 25)
+p.moveRect(5)
+cv2.waitKey(0)
